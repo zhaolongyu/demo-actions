c424c04b1074ca5d16faf9c53b382a068a2fb64e
zhaoyl <1486607544@qq.com>
2020-03-31 14:34:04 +0800

    jenkins

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,51 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
-
-export default new Vuex.Store({
+const modulesA = {
+  namespaced: true,
   state: {
+    list: 1
   },
   mutations: {
+    savelist () {
+      console.log(222222222222222)
+    },
+    savelist123123 () {
+      console.log(222222222222222)
+    },
+    dsfsd123 () {
+      console.log(222222222222222)
+    }
   },
   actions: {
+    saveSessionId ({ commit }, payload) {
+      commit('savelist')
+      console.log({ commit }, payload)
+    }
+  }
+}
+
+export default new Vuex.Store({
+  namespaced: true,
+  state: {
+    token: false,
+    test: 2222
+  },
+  mutations: {
+    tokenBooleanFalse () {
+      console.log(11111111111)
+      this.state.token = false
+    },
+    tokenBooleanTrue () {
+      this.state.token = true
+    }
+  },
+  actions: {
+    add: async ({ commit }) => {
+      commit('tokenBooleanFalse')
+    }
   },
   modules: {
+    modulesA
   }
 })
